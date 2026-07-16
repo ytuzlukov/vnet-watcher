cd65ff75774fc90121057c1df213e5f13f2c4c2e
Yan <tyzluckoff@yandex.ru>
2026-07-17 03:37:41 +0700

    test

--- src/main/java/net/valut/watcher/AppConfig.java
+++ src/main/java/net/valut/watcher/AppConfig.java
@@ -18,7 +18,7 @@ public record AppConfig(
 ) {
     private static final String DEFAULT_SITES = String.join(",",
             "https://valut.net",
-            "https://www.instagram.com",
+            "https://service-unavailable.invalid",
             "https://pro.valut.net",
             "https://docs.valut.net",
             "https://app.travel.cards"
